ae9f024452c79623c535b32d8b475b5b2aff989f
Aime Patrick <aimepatrick64@gmail.com>
2025-05-14 21:46:54 +0200

    update .env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string, );
+    const UrlDb = process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI_PROD
+    : process.env.MONGO_URI_DEV;
+    const conn = await mongoose.connect(UrlDb as string, );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     if (error instanceof Error) {
